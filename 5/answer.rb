34bc0860b9ce6ceba0b95e5142569b42b044b4cf
kenji <kenjix12x22@gmail.com>
2021-12-06 14:56:02 +0900

    5

---
+++ 5/answer.rb
@@ -0,0 +1,6 @@
+n = gets.to_i
+
+arr = Array.new(n)
+n.times { |i| arr[i] = 'paiza' }
+
+puts arr.join(' ')
---
+++ 5/my.rb
@@ -0,0 +1,6 @@
+n = gets.to_i
+ans = 'paiza'
+(n - 1).times do
+    ans += ' paiza'
+end
+puts ans
